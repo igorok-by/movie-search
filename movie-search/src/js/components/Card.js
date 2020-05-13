7295f770d71fd3bcfb6bab6f0d2cbb4d25cf0b7a
Ihar Bachkouski <igorko.by@gmail.com>
2020-05-13 14:32:53 +0300

    feat: add placeholder for empty images

--- movie-search/src/js/components/Card.js
+++ movie-search/src/js/components/Card.js
@@ -5,12 +5,13 @@ export default class Card {
     name, linkForName, linkOfPoster, yearOfRelease, ratingIMDb,
   }) {
     this.name = create('a', 'card__name', `${name}`, null, ['href', linkForName], ['target', 'blank']);
-    this.defaultPoster = 'https://images.unsplash.com/photo-1582450723295-c55c32a53428?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80';
+    this.defaultPoster = '/assets/img/no-image-icon.png';
     this.linkOfPoster = linkOfPoster === 'N/A' ? this.defaultPoster : linkOfPoster;
     this.poster = create('img', 'card__img', null, null, ['src', this.linkOfPoster], ['alt', `The movie titled: ${name}`]);
     this.yearOfRelease = create('p', 'card__date', `${yearOfRelease}`, null);
-    this.rating = create('p', 'card__date card__date--stars', `${ratingIMDb}`, null);
-    this.subContainer = create('div', 'card', [this.name, this.poster, this.yearOfRelease, this.rating], null);
+    this.rating = create('p', 'card__date card__date--stars', `${ratingIMDb}`);
+    this.cardFooter = create('div', 'card__footer', [this.yearOfRelease, this.rating]);
+    this.subContainer = create('div', 'card', [this.name, this.poster, this.cardFooter]);
     this.container = create('div', 'swiper-slide', this.subContainer, null);
   }
 }
